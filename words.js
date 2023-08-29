54f1815f366defebff41cf3cf417df4bc6617448
mickeybot-79 <91163243+mickeybot-79@users.noreply.github.com>
2023-08-29 20:48:23 -0300

    Update words.js

--- words.js
+++ words.js
@@ -1,9 +1,7 @@
 const spanishWords = require('an-array-of-spanish-words')
 
 const wordFinder = (input, length) => {
-
     const filteredWords = spanishWords.filter(word => word.length === parseInt(length))
-
     const solutionArray = []
 
     for (let i = 0; i < filteredWords.length; i++) {
@@ -15,22 +13,14 @@ const wordFinder = (input, length) => {
             if (!input.includes(currentLetter)) {
                 notExcluded = false
                 break
-            }
-        }
-        if (notExcluded) {
-            for (let k = 0; k < currentWord.length; k++) {
-                const currentLetter = currentWord[k]
+            } else {
                 const amountInCurrentWord = currentWord.split('').filter(letter => letter === currentLetter).length
                 const amountInInput = input.split('').filter(letter => letter === currentLetter).length
                 if (amountInCurrentWord > amountInInput) {
-                    letterAmount = false
-                    break
+                    solutionArray.push(currentWord)
                 }
-            }
-            if (letterAmount) solutionArray.push(currentWord)
         }
     }
-
     return solutionArray
 }
 

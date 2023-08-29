43a42744112fee9784d108fa7dae65966d4c1ec0
mickeybot-79 <91163243+mickeybot-79@users.noreply.github.com>
2023-08-29 20:50:01 -0300

    Update words.js

--- words.js
+++ words.js
@@ -6,12 +6,11 @@ const wordFinder = (input, length) => {
 
     for (let i = 0; i < filteredWords.length; i++) {
         const currentWord = filteredWords[i]
-        var notExcluded = true
-        var letterAmount = true
+        var valid = true
         for (let j = 0; j < currentWord.length; j++) {
             const currentLetter = currentWord[j]
             if (!input.includes(currentLetter)) {
-                notExcluded = false
+                valid = false
                 break
             } else {
                 const amountInCurrentWord = currentWord.split('').filter(letter => letter === currentLetter).length

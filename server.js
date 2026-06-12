a633b7ba02df5856e9d345f578d9f99504337cb9
mickeybot-79 <michaelperezvezoli@hotmail.com>
2026-06-12 12:51:43 -0300

    updated repo

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const wordFinder = require('./words')
+
+app.use(cors())
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile('/index.html', {root: __dirname})
+    res.sendFile('/style.css', {root: __dirname})
+    res.sendFile('/main.js', {root: __dirname})
+})
+
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/wake', async (req, res) => {
+    res.send('ready')
+})
+
+app.post('/words', (req, res) => {
+    const { input, length, language } = req.body
+    //console.log(input, length, language)
+    const result = wordFinder(input.toLowerCase(), length, language)
+    res.send(result)
+})
+
+//const hostname = '192.168.1.10'
+
+app.listen(5000, () => console.log(`Server running on port 5000`))
